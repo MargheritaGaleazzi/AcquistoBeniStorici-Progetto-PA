6ce9bfe9f6d0729ff9a1d527a9cda5c43803eea2
ChiaraAmalia <chiara.caporusso@outlook.it>
2022-06-22 09:19:37 +0200

    ora docker va finalmente

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,7 @@
 FROM node:lts-stretch-slim
 WORKDIR /usr/src/app
-
-#COPY package.json ./
 COPY . .
-
 RUN npm install
-
-#RUN npm install pm2 -g
-
-
-#RUN npm run build
-
-#COPY ./dist .
-
-#EXPOSE 4000
-
-#CMD ["pm2-runtime","app.js"]
-
 RUN npm install -g typescript
-
 RUN tsc
-
 CMD ["node", "app.js"]
--- connessione.ts
+++ connessione.ts
@@ -1,61 +1,21 @@
 require('dotenv').config();
 import {Sequelize} from 'sequelize';
 
-const fs = require('fs');
-const mysql = require("mysql2");
-
-// Open the connection to MySQL server
-const connection = mysql.createConnection({
-  multipleStatements: true,
-  host: "localhost",
-  user: "root",
-  password: "",
-});
-
-// Run create database statement
-connection.query(
-  `CREATE DATABASE IF NOT EXISTS gestione_acquisto_beni`,
-  function (err:any, results:any) {
-    console.log(results);
-    console.log(err);
-  }
-);
-const seedQuery = fs.readFileSync("seeder/seed.sql",{
-        encoding:"utf-8",
-    })
-
-//const fs = require('fs');
-//const seedQuery = fs.readFileSync("seeder/seed.sql",{
-//    encoding:"utf-8",
-//})
-//
-
-
-// Close the connection
-connection.end();
 /*
  * La classe Singleton è quella che ci permette di avere un'unica
  * connessione al DB, ovvero una sola istanza di tale oggetto.
  */
 
-
 export class Singleton{
     private static istanza: Singleton;
     private connessione: Sequelize;
-    private MYSQL_DATABASE = "gestione_acquisto_beni"
-    private MYSQL_ROOT_PASSWORD="root"
-    private MYSQL_USER="user"
-    private MYSQL_PASSWORD="password"
-    private MYSQL_HOST="localhost"
-    private MYSQL_PORT=3306
-
 
     private constructor(){
-        this.connessione = new Sequelize(this.MYSQL_DATABASE,  'root', '',{
-            host: this.MYSQL_HOST,
-            port: this.MYSQL_PORT,
-            dialect: 'mysql'
-        });
+      this.connessione = new Sequelize(process.env.MYSQL_DATABASE!, process.env.MYSQL_USER!, process.env.MYSQL_PASSWORD, {
+        host: process.env.MYSQL_HOST,
+        port: Number(process.env.MYSQL_PORT),
+        dialect: 'mysql'
+      });
     }
 
     public static getConnessione(): Sequelize {
